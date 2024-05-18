b5e4721090370ff7257a54c50d74b284312baefb
user <lizasamsung98@gmail.com>
2024-05-18 18:19:42 +0300

    urls

---
+++ src/main/java/urls/Urls.java
@@ -0,0 +1,10 @@
+package urls;
+
+public class Urls {
+    public static final String zoo = "https://zoo.kyiv.ua";
+    public static final String schools = "https://www.w3schools.com/";
+    public static final String taxi = "https://taxi838.ua/ru/dnepr/";
+    public static final String klopotenko = "https://klopotenko.com/";
+    public static final String shop = "http://www.automationpractice.pl/index.php";
+
+}
